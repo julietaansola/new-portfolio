bef57711433c65146dabb5090a4fe63b6a96a1f9
julietaansola <julietaansolaberreaute@gmail.com>
2021-02-19 21:37:58 -0300

    Update redes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -38,8 +38,8 @@ class Header extends Component {
             <h3>{description}.</h3>
             <hr />
             <ul className="social">
-               <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
-               <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
+               <a href={linkedin} className="button btn project-btn" target="_blank"><i className="fa fa-book"></i>Linkedin</a>
+               <a href={github} className="button btn github-btn" target="_blank"><i className="fa fa-github"></i>Github</a>
             </ul>
          </div>
       </div>
